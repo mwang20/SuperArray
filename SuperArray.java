1724b052b085f5232bab596f1a94907508657902
mwang20 <mwang20@stuy.edu>
2020-11-04 08:35:07 -0500

    Created the add method. I hope to test it using the get method for the next commit and push.

--- SuperArray.java
+++ SuperArray.java
@@ -3,10 +3,18 @@ public class SuperArray{
   private String[] data;
   private int size;
   public SuperArray(){
-    String[] data = new String[10];
+    data = new String[10];
     size = 0;
   }
   public int size(){
     return size;
+  }
+  public boolean add(String element){
+    if (size < 10){
+      data[size] = element;
+      size += 1;
+      return true;
+    }
+    return false;
   }
 }
--- Tester.java
+++ Tester.java
@@ -2,5 +2,7 @@ public class Tester{
   public static void main(String[] args){
     SuperArray testSuper = new SuperArray();
     System.out.println(testSuper.size()); //Should return 0
+    testSuper.add("hello"); //Should add "hello" to index 0
+    testSuper.add("yolo"); //SHould add "yolo to index 1"
   }
 }
