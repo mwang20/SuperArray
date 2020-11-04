2a1c3306cf78c259393f0c2dcbc2b776752c999f
mwang20 <mwang20@stuy.edu>
2020-11-04 04:00:29 -0500

    Made the size method. When I tested it it printed out 0 as expected

--- SuperArray.java
+++ SuperArray.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
 public class SuperArray{
   private String[] data;
   private int size;
   public SuperArray(){
-    size = 10;
-    String[] data = new String[size];
+    String[] data = new String[10];
+    size = 0;
+  }
+  public int size(){
+    return size;
   }
 }
--- Tester.java
+++ Tester.java
@@ -1,4 +1,6 @@
 public class Tester{
   public static void main(String[] args){
+    SuperArray testSuper = new SuperArray();
+    System.out.println(testSuper.size()); //Should return 0
   }
 }
